9d21330ed2abb83568a22337225ffb38513c9f04
chris-15 <chrismac@Chriss-MacBook-Pro.local>
2022-03-09 17:29:01 -0500

    added local storage and fetch for store info

--- js/script.js
+++ js/script.js
@@ -3,9 +3,17 @@ var bingApiKey = "Ai5n4MIAAGi2oZlEeqeqwzpcluN00BmjulByA70pa5NlqQQUNlQdpBOSZ_lfbi
 var inputSearchEl=  document.querySelector("#search");
 var searchFormEl =  document.querySelector("#search-form");
 var foodCardContainerEl = document.querySelector("#food-card-container");
+var zipSearchFormEl = document.querySelector("#search-zip-form");
+var inputZipSearchEl = document.querySelector("#search-zip");
+var storeCardContainerEl= document.querySelector("#store-card container");
+var historyButtonContainerEl = document.querySelector("#history-button-container")
+
+
+// array for searches stored in local storage
+var historyFoodList= localStorage.getItem("userSearchTerm")?JSON.parse(localStorage.getItem("userSearchTerm")):[];
 
 // function to fetch search request for books with a subject search
-var findBooks = function (foodName) {
+var findFood = function (foodName) {
 
     //https://api.edamam.com/api/food-database/v2/parser?app_id=d7658b96&app_key=28c3ad5a64730b44c357576febfb2dd9&ingr=fries&nutrition-type=cooking
 
@@ -72,32 +80,61 @@ var findBooks = function (foodName) {
                     carbListItemEl.innerHTML= "Carbohydrates: " + carb + "g";
                     cardListEl.appendChild(carbListItemEl);
 
-
-
-
-
-
-
-
                 }
-
-
-
-
-
-
-
-
 
             })
         } 
     })
+};
+
+// function that creates new buttons for each search history when a new search is conducted
+var newHistoryButton = function() {
+    var newHistoryButtonEl =  document.createElement("button");
+    newHistoryButtonEl.setAttribute("type", "click");
+    newHistoryButtonEl.textContent= inputSearchEl.value.trim();
+    // add classes here for styling
+    historyButtonContainerEl.appendChild(newHistoryButtonEl);
+}
+
+// function to load the local storage so history stays on page if page is left or refreshed
+var loadHistory = function() {
+    for (i=0; i < historyFoodList.length; i++) {
+        var historyFoodItemEl = document.createElement("button");
+        historyFoodItemEl.setAttribute("type", "click");
+        // add classes here for styling
+        historyFoodItemEl.textContent= (historyFoodList[i]);
+        historyButtonContainerEl.appendChild(historyFoodItemEl);
+    }
 }
 
 
 // fucntion to fetch store search request for stores near location zip
-var findStores = function (){
+var findStores = function (zipCode) {
+    // bingApiUrlOne = "http://dev.virtualearth.net/REST/v1/Locations/US/zipCode/?&key=Ai5n4MIAAGi2oZlEeqeqwzpcluN00BmjulByA70pa5NlqQQUNlQdpBOSZ_lfbiyO"
 
+    var bingApiUrlOne = "http://dev.virtualearth.net/REST/v1/Locations/US/CT/" +zipCode + "/?&key=Ai5n4MIAAGi2oZlEeqeqwzpcluN00BmjulByA70pa5NlqQQUNlQdpBOSZ_lfbiyO";
+
+    fetch(bingApiUrlOne).then(function (response) {
+        if (response.ok) {
+            console.log("bing fetch worked");
+            response.json().then(function (data) {
+                //console.log(data);
+                var cityLat = data.resourceSets[0].resources[0].point.coordinates[0];
+                var cityLong= data.resourceSets[0].resources[0].point.coordinates[1];
+
+                bingApiUrlTwo= "https://dev.virtualearth.net/REST/v1/LocalSearch/?type=Grocers&userLocation="+cityLat+", "+cityLong+ "&key=Ai5n4MIAAGi2oZlEeqeqwzpcluN00BmjulByA70pa5NlqQQUNlQdpBOSZ_lfbiyO";
+
+                fetch(bingApiUrlTwo).then(function(response) {
+                    return response.json();
+                }).then(function(data) {
+                    console.log(data)
+                })
+
+            })
+        } else {
+            console.log("fetch didnt work")
+        }
+    })
 };
 
 //function to handle book form search
@@ -106,13 +143,49 @@ var foodFormHandler = function(event) {
 
     var foodSearch = inputSearchEl.value.trim();
 
+    // pushes value to array only if it has a value, no blanks
     if (foodSearch) {
-        findBooks(foodSearch);
-        inputSearchEl.value= "";
+        historyFoodList.push(foodSearch)
+    };
+
+    // sets local storage
+    localStorage.setItem("userSearchTerm", JSON.stringify(historyFoodList));
+
+    if (foodSearch) {
+        findFood(foodSearch);
+        newHistoryButton(foodSearch);
+        inputSearchEl.value= ""
     } else {
-        console.log("enter a valid subject")
+        console.log("enter a valid food")
+        // use a modal here for error
+    }
+};
+
+//function to handle store search 
+var storeSearchHandler = function(event) {
+    event.preventDefault();
+    var zipSearch = inputZipSearchEl.value.trim();
+
+    if (zipSearch) {
+        findStores(zipSearch);
+        inputZipSearchEl.value = "";
+    } else {
+        console.log("enter a valid zip")
         // use a modal here for error 
     }
 };
 
+// function to handle history button clicks
+var historyButtonHandler = function(event) {
+    console.log(event.target)
+    var foodBtn = event.target.textContent;
+    findFood(foodBtn);
+}
+
 searchFormEl.addEventListener("submit", foodFormHandler);
+
+zipSearchFormEl.addEventListener("submit", storeSearchHandler);
+
+historyButtonContainerEl.addEventListener("click", historyButtonHandler);
+
+loadHistory();
